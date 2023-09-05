d8a952f29133975b60beac112cfe0c5d4230466c
geunheejung <geunhee0212@gmail.com>
2023-09-05 13:43:52 +0900

    reactivity-in-depth 실습.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,9 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Vuex from "vuex";
+import Vue, { ref, watchEffect } from "vue";
 import router from "./routes";
 import App from "./App.vue";
 import store from "./store";
 
 Vue.config.productionTip = false;
-
-// 애플리케이션의 구조를 파악할 수 있는 청사진 -> 구조도가 눈에 잘 보여야 함.
-// Vue.use(VueRouter);
 
 const vue = new Vue({
   store,

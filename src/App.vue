b932c2f7794d869ca9ccb6a4e0853c17e754b3bf
geunheejung <geunhee0212@gmail.com>
2023-09-05 16:13:19 +0900

    라우트 트랜지션 적용.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar />
-    <router-view />
+    <transition name="page">
+      <router-view />
+    </transition>
   </div>
 </template>
 
@@ -22,5 +24,16 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+
+/* transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
 }
 </style>

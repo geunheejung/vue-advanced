869493f077c9f95d9a42b93c40fc1b48c129d3de
geunheejung <geunhee0212@gmail.com>
2023-09-05 09:54:12 +0900

    hacker-news-pwas/news 조회 api 테스트.

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -3,7 +3,23 @@
 </template>
 
 <script>
-export default {};
+import axios from "axios";
+
+export default {
+  name: "NewsView",
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    const vm = this;
+    const url = `https://api.hnpwa.com/v0/news/1.json`;
+    axios.get(url).then((res) => {
+      vm.users = res.data;
+    });
+  },
+};
 </script>
 
 <style></style>
